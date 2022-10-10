722ed14ec8e8d7e0c043c9145cde8390d6ef929a
Josh Flatt <josh.m.flatt@gmail.com>
2022-10-09 20:25:57 -0600

    HW 2 - created in BlueJ & Eclipse

---
+++ HomeworkAssignment2/Shape.java
@@ -0,0 +1,31 @@
+
+/**
+ * CS200 Programming Assignment 2
+ *
+ * @author Josh Flatt
+ * @version 10/09/2022
+ */
+
+public abstract class Shape {
+    
+    //Fields - measured in pounds
+    private double weight;
+    
+    //Constructors
+    public Shape() {
+        this.weight = 0.0;
+    }
+    public Shape(double weight) {
+        this.weight = weight;
+    }
+    
+    //Setters
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+    
+    //Getters
+    public double getWeight() {
+        return this.weight;
+    }
+}
