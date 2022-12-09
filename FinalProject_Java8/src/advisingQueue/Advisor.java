f1cacdbafe3f5f152f74551817532d36443870cc
josh-flatt <josh.m.flatt@gmail.com>
2022-12-08 20:37:24 -0700

    Implemented couple examples of javafx scenes

---
+++ FinalProject_Java8/src/advisingQueue/Advisor.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package advisingQueue;
+
+/**
+ * @author joshm
+ *
+ */
+public class Advisor extends Person {
+
+	public Advisor(String firstName, String lastName, String email) {
+		super(firstName, lastName, email);
+	}
+}
---
+++ FinalProject_Java8/src/advisingQueue/AdvisorQueue.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package advisingQueue;
+
+import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Queue;
+
+/**
+ * @author joshm
+ *
+ */
+public class AdvisorQueue {
+
+	private Queue<Student> studentQueue;
+	private Advisor advisor;
+	private Meeting currentMeeting;
+	private ArrayList<Meeting> meetings;
+	
+	public AdvisorQueue(ArrayList<Meeting> meetings, Advisor advisor) {
+		this.advisor = advisor;
+		this.studentQueue = new LinkedList<>();
+		this.currentMeeting = null;
+		this.meetings = meetings;
+	}
+	
+	// Queue actions
+	public void addStudent(Student student) {
+		studentQueue.add(student);
+	}
+	public void removeStudent(Student student) {
+		studentQueue.remove(student);
+	}
+	public Student peekNextStudent() {
+		return studentQueue.peek();
+	}
+	
+	// Meeting actions
+	public boolean startMeeting() {
+		try {
+			Student student = studentQueue.peek();
+			if (student.equals(null)) {
+				throw new Exception("There are no students in the Queue!");
+			}
+			if (!this.currentMeeting.equals(null)) {
+				throw new Exception("There is already a meeting in progress!");
+			}
+			this.currentMeeting = new Meeting(student, this.advisor);
+			studentQueue.poll();
+			return true;
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+			return false;
+		}
+	}
+	public boolean endMeeting() {
+		Meeting meeting = currentMeeting;
+		boolean success = meeting.endMeeting();
+		if (success) {
+			this.currentMeeting = null;
+			meetings.add(meeting);
+			return true;
+		}
+		return false;
+	}
+}
