34710d9ad2613ee10ac9017d7b528fcc5d1dd346
josh-flatt <josh.m.flatt@gmail.com>
2022-12-09 11:48:01 -0700

    further gui implementation
    Added advisor dropdown on student view, added buttons to advisor view

--- FinalProject_Java8/src/application/MainMenuController.java
+++ FinalProject_Java8/src/application/MainMenuController.java
@@ -3,6 +3,8 @@
  */
 package application;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -28,34 +30,20 @@ public class MainMenuController {
 	private AdvisorQueue advQC;
 	private AdvisorQueue advQD;
 	
-	@FXML
-	private Button button;
+	ObservableList<String> advisorList = FXCollections
+			.observableArrayList("A", "B", "C", "D");
 	
+	//Student Page
+	@FXML
+	private Button studentSubmitButton;
 	@FXML
 	private TextField studentFirstName;
-	
 	@FXML
 	private TextField studentLastName;
-	
 	@FXML
 	private TextField studentEmail;
-	
 	@FXML
 	private ComboBox<String> studentAdvisorSelection;
-	
-	@FXML
-	public void initialize() {
-		studentAdvisorSelection = new ComboBox<String>();
-		studentAdvisorSelection.getItems().addAll(
-            "A",
-            "B",
-            "C",
-            "D"
-        );   
-
-		studentAdvisorSelection.setValue("B");
-	}
-	
 	@FXML
 	protected void onStudentSubmitButtonClicked(ActionEvent event) {
 		String firstName = this.studentFirstName.getText();
@@ -65,5 +53,38 @@ public class MainMenuController {
 		Student student1 = new Student(firstName, lastName, email);
 		studentEmail.setText(student1.getFullName());
 	}
+	
+	
+	
+	
+	//Advisor CJ
+	@FXML
+	private Button advCJStartMeetingButton;
+	@FXML
+	private Button advCJEndMeetingButton;
+	@FXML
+	private Button advCJRefreshButton;
+	@FXML
+	private Label advCJQueueCtLabel;
+	@FXML
+	protected void onAdvCJRefreshButtonClicked(ActionEvent event) {
+		
+	}
+	@FXML
+	protected void onAdvCJStartMeetingButtonClicked(ActionEvent event) {
+		
+	}
+	@FXML
+	protected void onAdvCJEndMeetingButtonClicked(ActionEvent event) {
+		
+	}
+	
+	
+	@FXML
+	public void initialize() {
+		studentAdvisorSelection.setItems(advisorList);
+	}
+	
+	
 	
 }
